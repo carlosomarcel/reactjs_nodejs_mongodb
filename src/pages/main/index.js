48fe845be5a3a3aafa11f750c7521a057486326f
carlosomarcel <ca_marcel@hotmail.com>
2020-10-16 00:14:28 -0300

    Api Node - Aplicação Web React + NodeJs

---
+++ src/pages/main/index.js
@@ -0,0 +1,27 @@
+import React, { Component} from 'react';
+import api from "../../services/api";
+
+
+
+export default class Main extends Component {
+    state = {
+        products: [],
+
+    }
+    componentDidMount(){
+        this.loadProducts();
+
+    };
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        this.setState({products: response.data.docs});
+
+    };
+
+
+    render(){
+        return <h1>Contagem de Produtos:{this.state.products.length}</h1>
+    }
+}
